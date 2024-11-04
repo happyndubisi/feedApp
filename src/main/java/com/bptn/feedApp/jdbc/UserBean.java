2afe28c47b1d4dbf5b5b89ce890accdc55f039a9
Happiness Ndubisi <happyndubisi@gmail.com>
2024-11-04 16:44:01 -0500

    UserBean class creation

---
+++ src/main/java/com/bptn/feedApp/jdbc/UserBean.java
@@ -0,0 +1,20 @@
+package com.bptn.feedApp.jdbc;
+
+import java.security.Timestamp;
+
+public class UserBean {
+
+	public UserBean() {
+		private Integer userId;
+		private String firstName;
+		private String lastName;
+		private String username;
+		private String password;
+		private String phone;
+		private String emailId;
+		private Boolean emailVerified;
+		private Timestamp createdOn;
+		// TODO Auto-generated constructor stub
+	}
+
+}
