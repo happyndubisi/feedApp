c360ff5adb940570d8fa5d9175783f518dba0631
Happiness Ndubisi <happyndubisi@gmail.com>
2024-11-04 20:33:17 -0500

    UserController update and created user

--- src/main/java/com/bptn/feedApp/jdbc/UserMapper.java
+++ src/main/java/com/bptn/feedApp/jdbc/UserMapper.java
@@ -1,24 +1,32 @@
 package com.bptn.feedApp.jdbc;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.jdbc.core.RowMapper;
 
-class UserMapper implements RowMapper<UserBean>  {
+class UserMapper implements RowMapper<UserBean> {
 
-/* Create a UserBean object*/
-UserBean user = new UserBean();
+	@Override
+	public UserBean mapRow(ResultSet rs, int rowNum) throws SQLException {
 
-/* Populates the UserBean object with data from the resultSet */
-user.setUserId(rs.getInt("userId"));
-user.setFirstName(rs.getString("firstName"));
-user.setLastName(rs.getString("lastName"));
-user.setUsername(rs.getString("username"));
-user.setPassword(rs.getString("password"));
-user.setPhone(rs.getString("phone"));
-user.setEmailId(rs.getString("emailId"));
-user.setEmailVerified(rs.getBoolean("emailVerified"));
-user.setCreatedOn(rs.getTimestamp("createdOn"));
+		/* Create a UserBean object */
+		UserBean user = new UserBean();
 
-/* Return the populated UserBean object */
-return user;
+		/* Populates the UserBean object with data from the resultSet */
+		user.setUserId(rs.getInt("userId"));
+		user.setFirstName(rs.getString("firstName"));
+		user.setLastName(rs.getString("lastName"));
+		user.setUsername(rs.getString("username"));
+		user.setPassword(rs.getString("password"));
+		user.setPhone(rs.getString("phone"));
+		user.setEmailId(rs.getString("emailId"));
+		user.setEmailVerified(rs.getBoolean("emailVerified"));
+		user.setCreatedOn(rs.getTimestamp("createdOn"));
+
+		/* Return the populated UserBean object */
+		return user;
+
+	}
 
 }
