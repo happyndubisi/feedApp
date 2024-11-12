7b8978fdbb9fc4842d9876a99fe1ef1dc491ebab
Happiness Ndubisi <happyndubisi@gmail.com>
2024-11-12 11:33:25 -0500

    include the updateUserProfileo (Profile profile)method, updateUserProfile(Profile profile, User user) method in the UserService class

--- src/main/java/com/bptn/feedApp/jpa/User.java
+++ src/main/java/com/bptn/feedApp/jpa/User.java
@@ -2,47 +2,56 @@ package com.bptn.feedApp.jpa;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "user", schema = "public")
+@Table(name = "\"user\"")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "\"userId\"")
 	private Integer userId;
-
 	@Column(name = "\"firstName\"")
 	private String firstName;
-
 	@Column(name = "\"lastName\"")
 	private String lastName;
-
 	private String username;
-
 	@JsonProperty(access = Access.WRITE_ONLY)
 	private String password;
-
 	private String phone;
-
 	@Column(name = "\"emailId\"")
 	private String emailId;
-
 	@Column(name = "\"emailVerified\"")
 	private Boolean emailVerified;
-
 	@Column(name = "\"createdOn\"")
 	private Timestamp createdOn;
+	@JsonInclude(Include.NON_NULL)
+	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private Profile profile;
+	@JsonIgnore
+	@OneToMany(mappedBy = "user")
+	private List<Feed> feeds;
+	@JsonIgnore
+	@OneToMany(mappedBy = "user")
+	private List<FeedMetaData> feedMetaData;
 
 	public User() {
 	}
@@ -119,11 +128,34 @@ public class User implements Serializable {
 		this.createdOn = createdOn;
 	}
 
+	public Profile getProfile() {
+		return profile;
+	}
+
+	public void setProfile(Profile profile) {
+		this.profile = profile;
+	}
+
+	public List<Feed> getFeeds() {
+		return feeds;
+	}
+
+	public void setFeeds(List<Feed> feeds) {
+		this.feeds = feeds;
+	}
+
+	public List<FeedMetaData> getFeedMetaData() {
+		return feedMetaData;
+	}
+
+	public void setFeedMetaData(List<FeedMetaData> feedMetaData) {
+		this.feedMetaData = feedMetaData;
+	}
+
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", username="
 				+ username + ", password=" + password + ", phone=" + phone + ", emailId=" + emailId + ", emailVerified="
 				+ emailVerified + ", createdOn=" + createdOn + "]";
 	}
-
 }
